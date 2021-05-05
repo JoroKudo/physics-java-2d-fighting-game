f76d0c5c595688d5b6a9768f7522c20e9d648f91
Surber Fabian <fabian.surber@bbcag.ch>
2021-05-05 08:35:31 +0200

    Code for json request (initialized in app class for now)

--- src/main/java/application/main/App.java
+++ src/main/java/application/main/App.java
@@ -2,17 +2,22 @@ package application.main;
 
 import application.Navigation.Navigator;
 import application.Navigation.SceneType;
+import application.database.Request;
 import application.gui.*;
 import javafx.application.Application;
 
 
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         Navigator navigator = new Navigator(primaryStage);
+        Request request = new Request("a", "b", "c");
+        request.doRequest();
         navigator.registerScene(SceneType.WELCOME_SCENE, new WelcomeScene(navigator));
         navigator.registerScene(SceneType.GAME_SCENE, new GameScene(navigator));
         navigator.registerScene(SceneType.GAME_WIN_SCENE, new GameWinScene(navigator));
