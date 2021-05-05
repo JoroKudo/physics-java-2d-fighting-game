5c75efa7f5292dada57014dbd78ba3d48349ffe1
Surber Fabian <fabian.surber@bbcag.ch>
2021-05-05 10:33:21 +0200

    funtion for json get requests

--- src/main/java/application/database/Request.java
+++ src/main/java/application/database/Request.java
@@ -1,12 +1,11 @@
 package application.database;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+
 
 public class Request {
 
@@ -20,46 +19,43 @@ public class Request {
         this.input = input;
     }
 
-    public void doRequest() throws IOException {
-
-        URL url = new URL("https://ultimate-arena-2d-default-rtdb.europe-west1.firebasedatabase.app/.json");
-
+    public void doRequest() throws Exception {
+        URL url = new URL(urlString);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-        con.setRequestMethod("PUT");
-
-        con.setRequestProperty("Content-Type", "application/json; utf-8");
-
-        con.setRequestProperty("Accept", "application/json");
-
-        con.setDoOutput(true);
-
-        String jsonInputString = "{\n" +
-                "    \"user\": {\n" +
-                "        \"fabian\": {\n" +
-                "            \"password\": \"1234\",\n" +
-                "            \"email\": \"fabian.surber@gmail.com\"\n" +
-                "        },\n" +
-                "        \"leo\": {\n" +
-                "            \"password\": \"abc\",\n" +
-                "            \"email\": \"leo.röttele@gmail.com\"\n" +
-                "        }\n" +
-                "    }\n" +
-                "}\n";
-
-        try (OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes("utf-8");
-            os.write(input, 0, input.length);
-        }
-
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
+        if (requestMethod.equals("PUT")) {
+            con.setRequestMethod("PUT");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+            String jsonInputString = input;
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
             }
-            System.out.println(response.toString());
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+            }
+        }
+        if (requestMethod.equals("GET")) {
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            String contentType = con.getHeaderField("Content-Type");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            System.out.println(content.toString());
         }
     }
 }
+
 
--- src/main/java/application/main/App.java
+++ src/main/java/application/main/App.java
@@ -14,10 +14,23 @@ import java.io.IOException;
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws Exception {
         Navigator navigator = new Navigator(primaryStage);
-        Request request = new Request("a", "b", "c");
+        Request request = new Request("https://ultimate-arena-2d-default-rtdb.europe-west1.firebasedatabase.app/.json", "PUT", "{\n" +
+                "    \"user\": {\n" +
+                "        \"fabian\": {\n" +
+                "            \"password\": \"1234\",\n" +
+                "            \"email\": \"fabian.surber@gmail.com\"\n" +
+                "        },\n" +
+                "        \"leo\": {\n" +
+                "            \"password\": \"abc\",\n" +
+                "            \"email\": \"leo.röttele@gmail.com\"\n" +
+                "        }\n" +
+                "    }\n" +
+                "}");
         request.doRequest();
+        Request getRequest = new Request("https://ultimate-arena-2d-default-rtdb.europe-west1.firebasedatabase.app/.json", "GET", "-");
+        getRequest.doRequest();
         navigator.registerScene(SceneType.WELCOME_SCENE, new WelcomeScene(navigator));
         navigator.registerScene(SceneType.GAME_SCENE, new GameScene(navigator));
         navigator.registerScene(SceneType.GAME_WIN_SCENE, new GameWinScene(navigator));
