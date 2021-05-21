3e80ba4d0d979546b3f0a1602934e29dd4c7d2a9
Surber Fabian <fabian.surber@bbcag.ch>
2021-05-21 14:05:02 +0200

    get users

--- src/main/java/application/gui/LeaderboardScene.java
+++ src/main/java/application/gui/LeaderboardScene.java
@@ -11,12 +11,18 @@ import javafx.scene.input.KeyCode;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class LeaderboardScene extends BaseScene  {
     public LeaderboardScene(Navigator navigator) throws IOException {
         super(navigator, Images.leaderboard);
 
         Firebase firebase = new Firebase();
+
+        ArrayList<String> players = new ArrayList<>();
+        for (String player : players) {
+            System.out.println(player);
+        }
 
         firebase.addWin("Fabian");
         System.out.println(firebase.getWins("Fabian"));
